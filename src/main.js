e8461d160bebdab99b3233212909f27380cdbf50
rafirubayet <rafi.rubayet.jp@gmail.com>
2024-12-17 13:21:37 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import 'primeicons/primeicons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import { i18n } from "./i18n";
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+import router from './router';
 
 const app = createApp(App);
 app.use(i18n);
+app.use(router);
+app.use(Toast);
 app.mount("#app");

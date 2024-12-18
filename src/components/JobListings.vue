fb5dff604c97be6bd602baf6baf9558af220bd1c
rafirubayet <rafi.rubayet.jp@gmail.com>
2024-12-19 02:56:27 +0900

    Update JobListings.vue

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,35 +1,65 @@
 <script setup>
-import jobData from "@/jobs.json";
-import { ref, defineProps } from "vue";
+import { RouterLink } from 'vue-router';
 import JobListing from './JobListing.vue';
+import { reactive, defineProps, onMounted } from 'vue';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+import axios from 'axios';
+import { useI18n } from "vue-i18n";
+
+const { t, locale } = useI18n({ useScope: "global" });
 
 defineProps({
   limit: Number,
   showButton: {
     type: Boolean,
     default: false,
-  }
+  },
 });
 
-const jobs = ref(jobData);
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('/api/jobs');
+    state.jobs = response.data;
+  } catch (error) {
+    console.error('Error fetching jobs', error);
+  } finally {
+    state.isLoading = false;
+  }
+});
 </script>
 
 <template>
   <section class="bg-blue-50 px-4 py-10">
     <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-sky-500 mb-6 text-center">
-        Browse Jobs
+        {{ t('label.browse_jobs') }}
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+      <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+
+      <!-- Shoe job listing when done loading -->
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
+          :key="job.id"
+          :job="job"
+        />
       </div>
     </div>
   </section>
+
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
-        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+    <RouterLink
+      to="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >{{ t('button.view_all_jobs') }}</RouterLink
+    >
+  </section>
 </template>

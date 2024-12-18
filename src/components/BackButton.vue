5ca38f22b0632486a3abd01533ed3da504c013de
rafirubayet <rafi.rubayet.jp@gmail.com>
2024-12-19 01:29:59 +0900

    Create BackButton.vue

---
+++ src/components/BackButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import { useI18n } from "vue-i18n";
+
+const { t, locale } = useI18n({ useScope: "global" });
+</script>
+
+<template>
+  <section>
+    <div class="container m-auto py-6 px-6">
+      <RouterLink
+        to="/jobs"
+        class="text-sky-500 hover:text-sky-600 flex items-center"
+      >
+        <i class="pi pi-arrow-circle-left mr-3"></i> {{ t('button.back_btn_msg') }}
+      </RouterLink>
+    </div>
+  </section>
+</template>

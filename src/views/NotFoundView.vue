7328aacda0e61c1678b912f38a39635b209a4df7
rafirubayet <rafi.rubayet.jp@gmail.com>
2024-12-19 13:56:58 +0900

    Add not found page

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import { useI18n } from "vue-i18n";
+
+const { t, locale } = useI18n({ useScope: "global" });
+</script>
+
+<template>
+  <section class="text-center flex flex-col justify-center items-center h-96">
+    <i class="pi pi-exclamation-triangle text-yellow-500 text-7xl mb-5"></i>
+    <h1 class="text-6xl font-bold mb-4">404</h1>
+    <p class="text-xl mb-5">{{ t('label.pageNotExist') }}</p>
+    <RouterLink
+      to="/"
+      class="text-white bg-sky-700 hover:bg-sky-900 rounded-md px-3 py-2 mt-4"
+      >{{ t('button.pageNotExist') }}</RouterLink
+    >
+  </section>
+</template>
